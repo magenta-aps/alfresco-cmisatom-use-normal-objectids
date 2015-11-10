7ab317f1eeba828c9e827eb406ff252536692585
Seth Yastrov <seth@magenta.dk>
2015-11-10 14:26:37 +0100

    Initial commit

---
+++ set_java_7.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
---
+++ src/main/java/dk/magenta/cmisatom/MyCMISConnector.java
@@ -0,0 +1,35 @@
+package dk.magenta.cmisatom;
+
+import org.alfresco.opencmis.CMISConnector;
+import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+/**
+ * Created by syastrov on 11/10/15.
+ */
+public class MyCMISConnector extends CMISConnector {
+    @Override
+    public String constructObjectId(AssociationRef assocRef, String versionLabel, boolean dropStoreRef) {
+        return super.constructObjectId(assocRef, versionLabel, true);
+    }
+
+    @Override
+    public String constructObjectId(String incomingNodeId, String versionLabel, boolean dropStoreRef) {
+        return super.constructObjectId(incomingNodeId, versionLabel, true);
+    }
+
+    @Override
+    public String constructObjectId(NodeRef incomingNodeRef, String versionLabel, boolean dropStoreRef) {
+        return super.constructObjectId(incomingNodeRef, versionLabel, true);
+    }
+
+    @Override
+    public String createObjectId(NodeRef nodeRef, boolean dropStoreRef) {
+        return super.createObjectId(nodeRef, true);
+    }
+
+    @Override
+    public String getRepositoryId() {
+        return super.getRepositoryId();
+    }
+}
